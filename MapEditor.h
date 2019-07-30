cf91e8a716652e122d26de14a62f29e9eb58c849
GabeXxX <gabriele.bruni@outlook.com>
2019-07-30 15:20:45 +0200

    Add MapEditor

---
+++ MapEditor.h
@@ -0,0 +1,63 @@
+//
+// Created by Gabriele Bruni on 2019-07-30.
+//
+
+#ifndef ASTAR_MAPVISUALIZER_H
+#define ASTAR_MAPVISUALIZER_H
+
+
+#include <SFML/Graphics.hpp>
+#include "Grid.h"
+
+
+class MapEditor {
+private:
+    Grid grid;
+
+    const int  WINDOW_WIDTH = 1800;
+
+    const int WINDOW_HEIGHT = 1000;
+
+    const int TILE_THICKNESS = 5;
+
+    const int CONTROL_PANE_WIDTH = 400;
+
+    const int MAP_HEIGHT;
+
+    const int MAP_WIDTH;
+
+    const int TILE_WIDTH;
+
+    const int TILE_HEIGHT;
+
+    sf::Vector2f cellSize;
+
+    sf::RenderWindow window;
+
+    sf::RectangleShape **map;  //2d array contenente le tile della mappa
+
+    sf::RectangleShape tile = sf::RectangleShape(sf::Vector2f(TILE_WIDTH, TILE_HEIGHT));
+
+
+public:
+
+    /*Game();
+      void run();
+    */
+    MapEditor(Grid& grid);
+
+    void run();
+
+    void processEvents();
+
+    void update();
+
+    void render();
+
+    void addWalls();
+
+    void addForests();
+};
+
+
+#endif //ASTAR_MAPVISUALIZER_H
